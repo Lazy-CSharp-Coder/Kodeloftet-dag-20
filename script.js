9d06af7df3b6a5c406b144582bb9eec9d4bca280
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-16 10:14:06 +0200

    test

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const disneyCharacters = [
 //
 //
 
+console.log(disneyCharacters);
+console.log(disneyCharacters[4]);
+console.log(disneyCharacters[4].name);
+
 const food = {
   vegetables: [
     { name: "Carrot", color: "orange" },
